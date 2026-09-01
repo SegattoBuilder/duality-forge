f60a85d0b4c85d3962867daaa6214745dd77acb1
Segatto <segattod@amazon.com>
2026-09-01 16:24:30 -0600

    refactor: Updated Analytics link to new one

--- js/core/analytics.js
+++ js/core/analytics.js
@@ -2,5 +2,5 @@
 const s = document.createElement('script');
 s.type = 'module';
 s.src = 'https://static.cloudflareinsights.com/beacon.min.js';
-s.setAttribute('data-cf-beacon', '{"token": "3eabada164064538b3bf93b67c7ac3e0"}');
+s.setAttribute('data-cf-beacon', '{"token": "5e91a5c1ba154ac3aac372aa0d198474"}');
 document.head.appendChild(s);
